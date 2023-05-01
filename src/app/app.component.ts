10c0ed43b6ac16d3157221d6f49d948842a3df82
msecchu94 <52843843+msecchu94@users.noreply.github.com>
2023-05-01 20:05:03 -0300

    First commit and environment

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  constructor() {
+    console.log(environment.apiUrl);
+  }
+  title = 'course-junior';
+}
